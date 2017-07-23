2839ec8d76c545b7435148233108c2898d286c46
oxsoftdev <oxsoftdev@gmail.com>
2017-07-22 21:20:49 -0700

    add(setup.py)

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dtbot'
+    , version='0.1'
+    , author='sebastian'
+    , author_email='oxsoftdev@gmail.com'
+    , packages=find_packages()
+    , url='https://github.com/oxsoftdev/dt-bot'
+    , license='LICENSE.txt'
+    , install_requires=[]
+    , dependency_links=[
+        'https://github.com/oxsoftdev/design-patterns-py/tarball/master'
+    ]
+)
+
